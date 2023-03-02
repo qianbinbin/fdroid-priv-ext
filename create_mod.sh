676453137945a54f12982782d31536da77e1a2e4
Binbin Qian <qianbinbin@hotmail.com>
2023-03-02 14:55:38 +0800

    Support for early exit

--- create_mod.sh
+++ create_mod.sh
@@ -4,6 +4,34 @@ set -e
 
 error() { echo "$@" >&2; }
 
+USAGE=$(
+  cat <<-END
+Usage: $0 [OPTION]...
+
+  -e                  early exit if no updates
+  -h                  display this help and exit
+
+Home page: <https://github.com/qianbinbin/fdroid-priv-ext>
+END
+)
+
+_exit() {
+  error "$USAGE"
+  exit 2
+}
+
+EARLY_EXIT=false
+while getopts "eh" c; do
+  case $c in
+  e) EARLY_EXIT=true ;;
+  h) error "$USAGE" && exit ;;
+  *) _exit ;;
+  esac
+done
+
+shift $((OPTIND - 1))
+[ $# -ne 0 ] && _exit
+
 PROG_DIR=$(dirname "$(realpath "$0")")
 cd "$PROG_DIR"
 OUT_DIR="$PROG_DIR/build"
@@ -22,7 +50,7 @@ FPE_OTA_OLD_VC=$(sed -n 's/^fpeOtaVersionCode=//p' module.prop)
 FPE_OTA_NEW_VC=$(curl -fsSL "https://f-droid.org/api/v1/packages/$FPE_OTA_PKG" | jq '.suggestedVersionCode')
 FDROID_OLD_VC=$(sed -n 's/^fdroidVersionCode=//p' module.prop)
 FDROID_NEW_VC=$(curl -fsSL "https://f-droid.org/api/v1/packages/$FDROID_PKG" | jq '.suggestedVersionCode')
-MOD_NEW_VER=$(date +%Y%m%d)
+MOD_VER=$(sed -n 's/^versionCode=//p' module.prop)
 
 UPGRADE=false
 if [ "$FPE_OTA_OLD_VC" -eq "$FPE_OTA_NEW_VC" ]; then
@@ -39,7 +67,8 @@ else
   error "$FDROID_PKG $FDROID_OLD_VC -> $FDROID_NEW_VC"
   UPGRADE=true
 fi
-[ "$UPGRADE" != true ] && exit 1
+[ "$EARLY_EXIT" = true ] && [ "$UPGRADE" != true ] && exit
+[ "$UPGRADE" = true ] && MOD_VER=$(date +%Y%m%d)
 
 error "==> Fetching info"
 INDEX_URL="https://f-droid.org/repo/index-v1.json"
@@ -70,25 +99,31 @@ chmod +x "$DUMB_DIR/update-binary"
 echo "#MAGISK" >"$DUMB_DIR/updater-script"
 chmod +x "$DUMB_DIR/updater-script"
 
-sed -e "s/^version=.*$/version=$MOD_NEW_VER/g" \
-  -e "s/^versionCode=.*$/versionCode=$MOD_NEW_VER/g" \
-  -e "s/^fpeOtaVersionCode=.*$/fpeOtaVersionCode=$FPE_OTA_NEW_VC/g" \
-  -e "s/^fdroidVersionCode=.*$/fdroidVersionCode=$FDROID_NEW_VC/g" module.prop >"$TMP_DIR/module.prop"
-cp "$TMP_DIR/module.prop" module.prop
 cp customize.sh "$TMP_DIR"
+if [ "$UPGRADE" = true ]; then
+  sed -e "s/^version=.*$/version=$MOD_VER/g" \
+    -e "s/^versionCode=.*$/versionCode=$MOD_VER/g" \
+    -e "s/^fpeOtaVersionCode=.*$/fpeOtaVersionCode=$FPE_OTA_NEW_VC/g" \
+    -e "s/^fdroidVersionCode=.*$/fdroidVersionCode=$FDROID_NEW_VC/g" module.prop >"$TMP_DIR/module.prop"
+  cp "$TMP_DIR/module.prop" module.prop
+else
+  cp module.prop "$TMP_DIR"
+fi
 
 error "==> Creating zip"
-FPE_MOD_ZIP="$FPE_PKG.mod_$MOD_NEW_VER.zip"
+FPE_MOD_ZIP="$FPE_PKG.mod_$MOD_VER.zip"
 cd "$TMP_DIR"
 zip -r "$OUT_DIR/$FPE_MOD_ZIP" .
 cd "$PROG_DIR"
 error "$OUT_DIR/$FPE_MOD_ZIP"
 
-error "==> Updating CHANGELOG"
-{
-  echo "### $MOD_NEW_VER" && echo
-  [ "$FPE_OTA_OLD_VC" -ne "$FPE_OTA_NEW_VC" ] && echo "- $FPE_OTA_PKG $FPE_OTA_OLD_VC -> $FPE_OTA_NEW_VC" && echo
-  [ "$FDROID_OLD_VC" -ne "$FDROID_NEW_VC" ] && echo "- $FDROID_PKG $FDROID_OLD_VC -> $FDROID_NEW_VC" && echo
-} >"$TMP_DIR/CHANGELOG.md"
-[ -f CHANGELOG.md ] && cat CHANGELOG.md >>"$TMP_DIR/CHANGELOG.md"
-mv "$TMP_DIR/CHANGELOG.md" CHANGELOG.md
+if [ "$UPGRADE" = true ]; then
+  error "==> Updating CHANGELOG"
+  {
+    echo "### $MOD_VER" && echo
+    [ "$FPE_OTA_OLD_VC" -ne "$FPE_OTA_NEW_VC" ] && echo "- $FPE_OTA_PKG $FPE_OTA_OLD_VC -> $FPE_OTA_NEW_VC" && echo
+    [ "$FDROID_OLD_VC" -ne "$FDROID_NEW_VC" ] && echo "- $FDROID_PKG $FDROID_OLD_VC -> $FDROID_NEW_VC" && echo
+  } >"$TMP_DIR/CHANGELOG.md"
+  [ -f CHANGELOG.md ] && cat CHANGELOG.md >>"$TMP_DIR/CHANGELOG.md"
+  mv "$TMP_DIR/CHANGELOG.md" CHANGELOG.md
+fi
